bd9e7f4afff46827a2656e0f4a98f632b754ce59
Cong Wang <congwang89@outlook.com>
2022-02-21 19:51:22 +0100

    Active moving change

--- data_utils/SeperateDatasetRandom.py
+++ data_utils/SeperateDatasetRandom.py
@@ -49,9 +49,17 @@ def seperate_dataset(root_dir, num_test, move=True):
     for category in classes.keys():
         print(category)
         new_dir = root_dir/Path(category)
+        tmp_dir = root_dir/Path(category)/"Train"
         create_dir(new_dir, "Train")
         create_dir(new_dir, "Test")
+        create_dir(tmp_dir, "No_use")
 
+        for i in range(num_test):
+            fileName = random.choice(os.listdir(tmp_dir))
+            if fileName.endswith('pcd'):
+                move_file(tmp_dir, "No_use", fileName, move)
+
+        """
         for i in range(num_test):
             fileName = random.choice(os.listdir(new_dir))
             if fileName.endswith('pcd'):
@@ -60,12 +68,13 @@ def seperate_dataset(root_dir, num_test, move=True):
         for other_files in os.listdir(new_dir):
             if other_files.endswith('pcd'):
                 move_file(new_dir, "Train", other_files)
+        """
 
 
 if __name__ == "__main__":
     # seperate_dataset("/home/airocs/Desktop/Dataset/Rotated_visual_data_pcd/", 25)
     # seperate_dataset("/home/airocs/Desktop/sampled_tactile_data_set", 20)
-    seperate_dataset("/home/airocs/Desktop/Dataset/tactile_pcd_10_sampled_21.02", 30, False)
+    seperate_dataset("/home/prajval/cong_workspace/Pointnet_Pointnet2_pytorch/data/active_vision_pcd_4000", 100, True)
 
 
 
