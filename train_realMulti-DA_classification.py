0c8ea7275da4ef9c66cb409c84cd57a42afb08aa
Cong Wang <congwang89@outlook.com>
2022-02-21 17:46:09 +0100

    Trained with 20 epochs

--- train_realMulti-DA_classification.py
+++ train_realMulti-DA_classification.py
@@ -43,8 +43,8 @@ def parse_args():
     parser.add_argument('--SO3_Rotation', action='store_true', default=False, help='arbitrary rotation in SO3')
     parser.add_argument('--DA_method', type=str, default="multi_coral_mmd", help='choose the DA loss function')
     parser.add_argument('--alpha', type=float, default=10, help='set the value of classification loss')
-    parser.add_argument('--lamda', type=float, default=0.5, help='set the value of CORAL loss')
-    parser.add_argument('--beta', type=float, default=0.5, help='set the value of MMD loss')
+    parser.add_argument('--lamda', type=float, default=5, help='set the value of CORAL loss')
+    parser.add_argument('--beta', type=float, default=5, help='set the value of MMD loss')
     return parser.parse_args()
 
 
