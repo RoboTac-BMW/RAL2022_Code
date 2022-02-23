5a9c6c21a508d5c791fc70e695a99f690e358d10
congw112358 <congwang89@outlook.com>
2022-02-23 14:04:13 +0100

    Fix Error. Change txt to json

--- data_utils/PCDLoader.py
+++ data_utils/PCDLoader.py
@@ -8,6 +8,8 @@ from scipy.spatial.transform import Rotation as R
 
 
 import torch
+import json
+import ast
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 
@@ -278,9 +280,13 @@ class PCDActiveVision(PCDPointCloudData):
         with open(self.active_path) as file:
             lines = [line.rstrip() for line in file]
             for i in range(self.active_sample_num):
-                self.files.append(lines[i])
+                # print(lines[i])
+                # print(type(lines[i]))
+                converted_string=json.loads(lines[i])
+                # print(converted_string)
+                # print(type(converted_string))
+                self.files.append(converted_string)
 
-        # print(self.files)
         print(len(self.files))
 
 
