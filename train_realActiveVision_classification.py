07681bbc5c82bff1ae473f0f2d08235cea3f7db9
congw112358 <congwang89@outlook.com>
2022-02-23 18:36:50 +0100

    Modify inputs

--- train_realActiveVision_classification.py
+++ train_realActiveVision_classification.py
@@ -30,7 +30,7 @@ def parse_args():
     parser.add_argument('--model', default='pointnet_cls', help='model name [default: pointnet_cls]')
     parser.add_argument('--num_category', default=15, type=int, help='training on real dataset')
     parser.add_argument('--num_ModelNet', default=0, type=int, choices=[0,10,40], help='Pre-trained num_category')
-    parser.add_argument('--epoch', default=10, type=int, help='number of epoch in training')
+    parser.add_argument('--epoch', default=5, type=int, help='number of epoch in training')
     parser.add_argument('--learning_rate', default=0.001, type=float, help='learning rate in training')
     parser.add_argument('--num_point', type=int, default=1024, help='Visual Point Number')
     parser.add_argument('--optimizer', type=str, default='Adam', help='optimizer for training')
@@ -119,11 +119,11 @@ def main(args):
 
     '''DATA LOADING'''
     log_string('Load dataset ...')
-    data_path = 'data/active_vision_pcd_1500/'
+    data_path = 'data/active_vision_pcd_150/'
     active_txt_path = '/home/airocs/Desktop/active_entropy_files.json'
 
     train_dataset = PCDActiveVision(root_dir=data_path,
-                                    active_path=active_txt_path, active_sample_num=150,
+                                    active_path=active_txt_path, active_sample_num=50,
                                     folder='Train',
                                     random_shuffle=args.random_shuffle)
 
