47a7d0c83fbe2b0eeec93fea7933823679042b6b
congw112358 <congwang89@outlook.com>
2022-02-24 14:21:23 +0100

    Modify input

--- test_TactileProbability_classification.py
+++ test_TactileProbability_classification.py
@@ -29,7 +29,7 @@ def parse_args():
     parser.add_argument('--batch_size', type=int, default=8, help='batch size in training')
     # parser.add_argument('--num_category', default=10, type=int, choices=[10, 40],  help='training on ModelNet10/40')
     parser.add_argument('--num_category', default=12, type=int, help='training on real dataset')
-    parser.add_argument('--num_point', type=int, default=1024, help='Point Number')
+    parser.add_argument('--num_point', type=int, default=50, help='Point Number')
     parser.add_argument('--log_dir', type=str, required=True, help='Experiment root')
     parser.add_argument('--use_normals', action='store_true', default=False, help='use normals')
     parser.add_argument('--use_uniform_sample', action='store_true', default=False, help='use uniform sampiling')
